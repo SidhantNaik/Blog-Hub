4d7dbcaef7155a2d4b8fcaaa798cb6c81bcf9290
Bhakti Patil <99464844+Bhakti0120@users.noreply.github.com>
2025-04-16 08:38:18 +0530

    Sign-up page implemented.
    Designed and developed the sign-up page, including form fields, validation, and user registration functionality.

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Card } from "@/components/ui/card";
+import { RouteSignIn } from "@/helpers/RouteName";
+import { data, Link } from "react-router-dom";
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const formSchema = z.object({
+    name: z.string().min(3, "Name must be at least 3 charchter long"),
+    email: z.string().email(),
+    password: z.string().min(8, "Password must be at least 8 charchter long."),
+    confirmPassword: z
+      .string()
+      .refine(
+        (data) => data.password === data.confirmPassword,
+        "Password and confirm Password must be same."
+      ),
+  });
 
-export default SignUp
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name:'',
+      email: '',
+      password: '',
+      confirmPassword:''
+    },
+  });
+
+  function onSubmit(values) {
+    console.log(values);
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <Card className="w-[400px] p-5">
+        <h1 className=" text-2xl font-bold text-center mb-5">
+          Create Your Account
+        </h1>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your name" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your email address"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your password" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="mb-3">
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter password again" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className=" mt-5">
+              <Button type="submit" className="w-full">
+                Sign Up
+              </Button>
+              <div className="mt-5x text-sm flex items-center justify-center gap-2">
+                <p>Already have account?</p>
+                <Link
+                  to={RouteSignIn}
+                  className=" text-blue-500 hover:underline"
+                >
+                  Sign In
+                </Link>
+              </div>
+            </div>
+          </form>
+        </Form>
+      </Card>
+    </div>
+  );
+};
+
+export default SignUp;
